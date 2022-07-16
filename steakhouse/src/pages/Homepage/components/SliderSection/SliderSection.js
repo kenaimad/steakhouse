af94d53e092ea94cf95d69748bc6c54bb47f549d
kenaimad <dosieniecki@gmail.com>
2022-07-16 17:38:52 +0200

    Set fontSize in Slider components to relative.

--- steakhouse/src/pages/Homepage/components/SliderSection/SliderSection.js
+++ steakhouse/src/pages/Homepage/components/SliderSection/SliderSection.js
@@ -1,6 +1,6 @@
 import OrderButton from '../../../../utils/OrderButton/OrderButton';
 import * as s from './SliderSection.styled';
-import img from './images/Bitmap@2x.png';
+import img from "./images/Bitmap@2x.png";
 
 const SliderSection = () => {
   return (
@@ -12,21 +12,21 @@ const SliderSection = () => {
               <span>Rabat 10%</span> na pierwsze zamówienie!
               <p>
                 Do 20 stycznia złóż zamówienie na stronie i skorzystaj z
-                <b> promocji -10%</b>. Spiesz się. Czas trwania promocji do
-                końca lutego.
+                <b> promocji -10%</b>. Spiesz się. Czas trwania promocji do końca
+                lutego.
               </p>
             </h1>
             <OrderButton />
             <s.DotsContainer>
-              <div />
-              <div />
-              <div />
+                <div />
+                <div />
+                <div />
             </s.DotsContainer>
           </s.Description>
         </s.DescriptionContainer>
-        <s.ImageContainer>
-          <img src={img} alt="Steak" />
-        </s.ImageContainer>
+                  <s.ImageContainer>
+            <img src={img} alt="Steak" />
+          </s.ImageContainer>
       </s.SliderSectionStyled>
     </>
   );
--- steakhouse/src/pages/Homepage/components/SliderSection/SliderSection.styled.js
+++ steakhouse/src/pages/Homepage/components/SliderSection/SliderSection.styled.js
@@ -4,53 +4,54 @@ import { Colors } from '../../../../utils/palette';
 export const SliderSectionStyled = styled.section`
   display: flex;
   width: 100%;
-  height: 70%;
-  background-color: pink;
   position: relative;
+  
 `;
 
 export const DescriptionContainer = styled.div`
   width: 50%;
   height: 100%;
+  overflow: hidden;
   background-color: ${Colors.primaryBackground};
 `;
 
 export const Description = styled.div`
   position: absolute;
-  left: 149px;
+  left: 5%;
   top: 10%;
+  
 
   h1 {
-    font-size: 56px;
+    font-size: 2.6vw;
     width: 60%;
 
     p {
-        font-size: 20px;
-        width: 60%;
-        font-weight: 300;
+      font-size: 1vw;
+      width: 60%;
+      font-weight: 300;
     }
 
     span {
-        display: inline-block;
-        color: white;
-        background-color: black;
+      display: inline-block;
+      color: white;
+      background-color: black;
     }
   }
 `;
 
 export const DotsContainer = styled.div`
-    display: flex;
-    margin-top: 5%;
-    div {
-        margin: 5px;
-        width: 8px;
-        height: 8px;
-        border-radius: 100%;
-        background-color: white;
-        border: 1px solid black;
-        cursor: pointer;
-    }
-`
+  display: flex;
+  margin-top: 5%;
+  div {
+    margin: 5px;
+    width: 8px;
+    height: 8px;
+    border-radius: 100%;
+    background-color: white;
+    border: 1px solid black;
+    cursor: pointer;
+  }
+`;
 
 export const ImageContainer = styled.div`
   height: 100%;
@@ -60,4 +61,4 @@ export const ImageContainer = styled.div`
     width: 100%;
     height: 100%;
   }
-`
+`;
